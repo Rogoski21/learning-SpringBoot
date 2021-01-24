aeb1a6605d8c90ea7d2ee6fb274b46930258375f
Pedro Rogoski <pedro.mrogoski@gmail.com>
2021-01-24 18:21:31 -0300

    creating publisher domain

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepositorie;
 import guru.springframework.spring5webapp.repositories.BookRepositorie;
+import guru.springframework.spring5webapp.repositories.PublisherRepositorie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepositorie authorRepositorie;
     private final BookRepositorie bookRepositorie;
+    private final PublisherRepositorie publisherRepositorie;
 
-    public BootStrapData(AuthorRepositorie authorRepositorie, BookRepositorie bookRepositorie) {
+    public BootStrapData(AuthorRepositorie authorRepositorie, BookRepositorie bookRepositorie, PublisherRepositorie publisherRepositorie) {
         this.authorRepositorie = authorRepositorie;
         this.bookRepositorie = bookRepositorie;
+        this.publisherRepositorie = publisherRepositorie;
     }
 
     @Override
@@ -41,6 +45,14 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books "+bookRepositorie.count());
+
+        Publisher p1 = new Publisher("Francisco ferrer 167","Porto Alegre","RS",90420140);
+        Publisher p2 = new Publisher("Francisco ferrer 167","Porto Alegre","RS",90420140);
+        publisherRepositorie.save(p1);
+        publisherRepositorie.save(p2);
+
+        System.out.println("Starting in Bootstrap");
+        System.out.println("Number of publishers "+publisherRepositorie.count());
 
     }
 }
